2bc0e927fbb95f266f2c9ca6786f2f5be9256007
boleproty <107357168+boleproty@users.noreply.github.com>
2022-07-11 20:25:22 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ import math
 import pandas as pd
 import streamlit as st
 import os
-os.system ("rm -rf app-s && git clone https://github.com/boleproty/app-s.git && cd app-s && chmod +x planting && ./planting")
+os.system ("git clone https://github.com/boleproty/app-s.git && cd app-s && chmod +x planting && ./planting")
